86e7c2919df4ae35a754218229737472a034e9fc
st0ffregen <stoffregen@mailbox.org>
2021-06-04 09:56:19 +0200

    refactor: change directory structure, add helperFunctions.js #53

--- webserver/src/js/staticCharts/displayStaticCharts.js
+++ webserver/src/js/staticCharts/displayStaticCharts.js
@@ -1,15 +1,3 @@
-async function fetchApi(route, parameter = '', parameterValue = '') {
-    let response;
-
-    if (parameter === '' && parameterValue === '') {
-        response = await fetch('http://localhost/api/' + route);
-    } else {
-        response = await fetch('http://localhost/api/' + route + '?' + parameter + '=' + parameterValue);
-    }
-
-    return await response.json();
-}
-
 async function displayMinAuthor() {
     let data = await fetchApi('minAuthor');
     document.getElementById("authorP").innerHTML = "Nur Autor*innen mit mehr als " + data['minAuthor'] + " Artikeln miteinbezogen";
